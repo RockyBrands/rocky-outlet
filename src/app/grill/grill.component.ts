acd179bf9841e612b92b2ff82ef7859d8aa97fc5
Levi Stroop <levi.stroop@rockybrands.com>
2019-08-23 11:11:18 -0400

    Menu changes

--- src/app/grill/grill.component.ts
+++ src/app/grill/grill.component.ts
@@ -21,37 +21,37 @@ export class GrillComponent {
   title = 'The Boot Grill';
   intro_image = "./assets/images/boot_grill.jpg";
 
-  monday_date = '07/08';
-  monday_main = 'Kielbasa';
-  monday_sides = 'roasted red potatoes, and mixed vegetables';
-  monday_soup = 'Broccoli Cheese';
+  monday_date = '08/19';
+  monday_main = "Steak";
+  monday_sides = 'mashed potatoes, gravy, and green beans';
+  monday_soup = "Chicken Noodle";
 
-  tuesday_date = '07/09';
-  tuesday_main = 'Stuffed Crab';
-  tuesday_sides = 'macaroni & cheese, and California vegetables';
-  tuesday_soup = 'Tomato Tortellini';
+  tuesday_date = '08/20';
+  tuesday_main = "Enchiladas";
+  tuesday_sides = "broccoli, yellow squash, carrots, and Mexican rice";
+  tuesday_soup = 'Gumbolaya';
 
-  wednesday_date = '07/10';
-  wednesday_main = 'Pizza Bar';
-  wednesday_sides = 'a fan favorite!';
-  wednesday_soup = 'Beef Barley';
+  wednesday_date = '08/21';
+  wednesday_main = 'Catfish';
+  wednesday_sides = 'mac & cheese, and hearty cole slaw';
+  wednesday_soup = 'Tomato Zucchini';
 
-  thursday_date = '07/11';
-  thursday_main = 'Bourbon Chicken';
-  thursday_sides = 'white rice, and broccoli';
-  thursday_soup = 'French Onion';
+  thursday_date = '08/22';
+  thursday_main = 'Chicken Mushroom Marinara';
+  thursday_sides = 'seasoned mixed vegetables, and garlic toast';
+  thursday_soup = 'Potato Chophouse';
 
-  friday_date = '07/12';
-  friday_main = 'Burger Bar';
-  friday_sides = 'lattice chips, and baked beans';
-  friday_soup = 'Minestrone';
+  friday_date = '08/23';
+  friday_main = 'BBQ Ribs';
+  friday_sides = 'baked beans, and mac & cheese';
+  friday_soup = 'Chicken Pot Pie';
 
-  saturday_date = '07/13';
+  saturday_date = '08/24';
 
-  sunday_date = '07/14';
+  sunday_date = '08/25';
 
-  burger_name = 'The Italian';
-  burger_pic = './assets/images/the-italian.jpg';
-  burger_description = 'We start off with an 8" sub bun topped with butter and parmesan cheese. Then we add 6 slices of salami, 6 slices of pepperoni, and 4 ounces of ham. Then we top that refluxer with provolone cheese and toast it. Served with a side of spicy Italian dressing that you will not want to pass on. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!';
+  burger_name = 'The Rocky Burger';
+  burger_pic = './assets/images/rocky_burger.png';
+  burger_description = 'We start off with a brioche bun, then we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with salt and pepper. Then we top that with cheddar cheese, bacon, an egg (cooked however you would like it), lettuce, tomato, and grilled onions. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!';
 }
 //then we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with a house blend of spices. We top that tasty boi with bacon, cheddar cheese, our house made Bubba's Kickin' Q sauce, lettuce, tomato, pickle, and onion. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!
