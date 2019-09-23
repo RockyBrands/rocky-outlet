ffb488bd763d403b02c29a0706351aa559f88c62
Levi Stroop <levi.stroop@rockybrands.com>
2019-09-23 09:58:25 -0400

    Boot Grill Menu week of 09-23

--- src/app/grill/grill.component.ts
+++ src/app/grill/grill.component.ts
@@ -21,37 +21,37 @@ export class GrillComponent {
   title = 'The Boot Grill';
   intro_image = "./assets/images/boot_grill.jpg";
 
-  monday_date = '08/19';
-  monday_main = "Steak";
-  monday_sides = 'mashed potatoes, gravy, and green beans';
-  monday_soup = "Chicken Noodle";
+  monday_date = '09/23';
+  monday_main = "Salmon";
+  monday_sides = 'loaded mashed potatoes, and asparagus';
+  monday_soup = "Chicken and Wild Rice";
 
-  tuesday_date = '08/20';
-  tuesday_main = "Enchiladas";
-  tuesday_sides = "broccoli, yellow squash, carrots, and Mexican rice";
-  tuesday_soup = 'Gumbolaya';
+  tuesday_date = '09/24';
+  tuesday_main = "Fried Pork Chops";
+  tuesday_sides = "au gratin potatoes, and mixed vegetables";
+  tuesday_soup = 'Beef Noodle';
 
-  wednesday_date = '08/21';
-  wednesday_main = 'Catfish';
-  wednesday_sides = 'mac & cheese, and hearty cole slaw';
-  wednesday_soup = 'Tomato Zucchini';
+  wednesday_date = '09/25';
+  wednesday_main = "General Tso's Shrimp";
+  wednesday_sides = 'rice, broccoli, and egg rolls';
+  wednesday_soup = 'White Chicken Chili';
 
-  thursday_date = '08/22';
-  thursday_main = 'Chicken Mushroom Marinara';
-  thursday_sides = 'seasoned mixed vegetables, and garlic toast';
-  thursday_soup = 'Potato Chophouse';
+  thursday_date = '09/26';
+  thursday_main = 'Chicken Cavatappi';
+  thursday_sides = 'garlic toast, and California vegetables';
+  thursday_soup = 'French Onion';
 
-  friday_date = '08/23';
-  friday_main = 'BBQ Ribs';
-  friday_sides = 'baked beans, and mac & cheese';
-  friday_soup = 'Chicken Pot Pie';
+  friday_date = '09/27';
+  friday_main = 'BBQ Pulled Chicken';
+  friday_sides = 'mac & cheese, and hearty cole slaw';
+  friday_soup = 'Broccoli Cheese';
 
-  saturday_date = '08/24';
+  saturday_date = '09/28';
 
-  sunday_date = '08/25';
+  sunday_date = '09/29';
 
-  burger_name = 'The Rocky Burger';
-  burger_pic = './assets/images/rocky_burger.png';
-  burger_description = 'We start off with a brioche bun, then we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with salt and pepper. Then we top that with cheddar cheese, bacon, an egg (cooked however you would like it), lettuce, tomato, and grilled onions. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!';
+  burger_name = 'The Bayou Burger';
+  burger_pic = './assets/images/bayou-burger.jpg';
+  burger_description = 'We start off with a Sunny Bun, and we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with cajun seasoning. We top that thing with gouda cheese, onion, pickle, lettuce, and tomato. We serve it up with a side of gator bits, breaded and seasoned with old bay. Consider adding hot sauce to your gator bites to give them back a little of their BITE.';
 }
 //then we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with a house blend of spices. We top that tasty boi with bacon, cheddar cheese, our house made Bubba's Kickin' Q sauce, lettuce, tomato, pickle, and onion. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!
