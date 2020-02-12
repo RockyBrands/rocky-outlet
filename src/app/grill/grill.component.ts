abb745f0645bf2d93a3bcacd25a5e964eea52431
Levi Stroop <levistroop@Levi-Stroop.local>
2020-02-12 15:06:57 -0500

    General Boot Grill Updates 02/12/2020

--- src/app/grill/grill.component.ts
+++ src/app/grill/grill.component.ts
@@ -21,37 +21,37 @@ export class GrillComponent {
   title = 'The Boot Grill';
   intro_image = "./assets/images/boot_grill.jpg";
 
-  monday_date = '09/30';
-  monday_main = "Burger Bar";
-  monday_sides = 'a fan favorite!';
-  monday_soup = "White Chicken Chili";
+  monday_date = '02/10';
+  monday_main = "Free drink with purchase of any sandwich and a side";
+ // monday_sides = 'chips, and cole slaw';
+  monday_soup = "Chicken Tortilla";
 
-  tuesday_date = '10/01';
-  tuesday_main = "Bourbon Chicken";
-  tuesday_sides = "rice, and broccoli";
-  tuesday_soup = 'Vegetable Soup';
+  tuesday_date = '02/11';
+  tuesday_main = "$1.00 off any side with purchase of a sandwich";
+ // tuesday_sides = "mashed potatoes, gravy, and corn";
+  tuesday_soup = 'Italian Wedding';
 
-  wednesday_date = '10/02';
-  wednesday_main = "Beef Stroganoff";
-  wednesday_sides = 'noodles, mashed potatoes, and corn';
-  wednesday_soup = 'Broccoli Cheese';
+  wednesday_date = '02/12';
+  wednesday_main = "Turkey";
+  wednesday_sides = "mashed potatoes, and gravy";
+  wednesday_soup = "Minestrone";
 
-  thursday_date = '10/03';
-  thursday_main = 'Chicken Alfredo';
-  thursday_sides = 'garlic toast, and spinach salad';
-  thursday_soup = 'Chicken Noodle';
+  thursday_date = '02/13';
+  thursday_main = 'Add fries for $1.00!';
+  //thursday_sides = 'au gratin potatoes, and green beans';
+  thursday_soup = 'French Onion';
 
-  friday_date = '10/04';
-  friday_main = 'Turkey';
-  friday_sides = 'mashed potatoes, gravy, and green beans';
-  friday_soup = 'Pumpkin & Roasted Apple with Bacon';
+  friday_date = '02/14';
+  friday_main = 'Chili Cheese Dog';
+  friday_sides = 'with fries, and a drink';
+  friday_soup = 'Beef Barley';
 
-  saturday_date = '10/05';
+  saturday_date = '02/15';
 
-  sunday_date = '10/06';
+  sunday_date = '02/16';
 
-  burger_name = 'The Chicken Fried';
-  burger_pic = './assets/images/chicky-chicky-fry-fry.jpg';
-  burger_description = "We start off with a Sunny Bun, and we add the 1/4lb Tyson no antibiotics added, breaded chicken breast patty, topped with pickles. We serve it up with sidewinder fries. It will make you wish you had a cold beer on a Friday night! It will also make your pants fit not quite right, but we don't have to think about that.";
+  burger_name = 'The Mystery Machine';
+  burger_pic = './assets/images/mystery-machine.jpg';
+  burger_description = "We take two slices of sourdough bread, slather them in butter, and toast them. We add a half pound of chopped steak then smother it with grilled onions, grilled mushrooms, and swiss cheese. Add your favorite side or stick with the delicious salty crunch of Conn's potato chips and it is a meal not soon to be forgotten!";
 }
 //then we add the 1/3lb. non-GMO burger patty from Symmes Creek Ranch in Patriot Ohio, seasoned with a house blend of spices. We top that tasty boi with bacon, cheddar cheese, our house made Bubba's Kickin' Q sauce, lettuce, tomato, pickle, and onion. Add your favorite side or stick with the delicious salty crunch of Conn’s Potato chips, and it is a meal not soon to be forgotten!
